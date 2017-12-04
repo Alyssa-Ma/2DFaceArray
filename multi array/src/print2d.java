368f01aed8e4eb7e37c622c45891a699ad41414b
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-12-04 09:25:48 -0500

    awewerawere

---
+++ multi array/src/print2d.java
@@ -0,0 +1,21 @@
+
+public class print2d {
+
+	int[][]2dArr = 
+	
+	
+	
+	
+	
+	public static void print2d(int[][]arr)
+	{
+		for(int i=0; i<arr.length; i++)
+		{
+			for(int j =0; j<arr[i].length; i++)
+			{
+				System.out.print(arr[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
