4526eb56df055e84af0a2cb305a9f44ed9ed2791
Alyssa <Alyssa@192.168.1.151>
2018-01-18 22:08:11 -0500

    . .  3

--- multi array/src/print2d.java
+++ multi array/src/print2d.java
@@ -1,19 +1,28 @@
-
-public class print2d {
-
-	int[][]2dArr = 
-	
-	
-	
-	
-	
-	public static void print2d(int[][]arr)
+public class print2d 
+{
+	public static void main(String[] args) 
 	{
-		for(int i=0; i<arr.length; i++)
+		
+		int [][] arr = new int [10][21];
+		arr[1][15] = 8;
+		arr[1][5] = 8;
+		arr[3][10] = 1;
+		arr[5][12] = 3;
+		
+		print(arr);
+	}
+	
+	public static void print(int [][] arr) 
+	{
+		for(int i = 0; i < arr.length; i++ )
 		{
-			for(int j =0; j<arr[i].length; i++)
+			for(int j = 0; j < arr[i].length; j++)
 			{
-				System.out.print(arr[i][j]);
+				if(arr[i][j] == 0)
+				{
+					System.out.print(" ");
+				}
+				else System.out.print(arr[i][j]);
 			}
 			System.out.println();
 		}
